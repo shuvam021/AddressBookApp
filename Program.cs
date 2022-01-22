ff64959fdaab7eea90a1cce425c2be7d6b2c128a
Shuvam Das <repository.shuvam@gmail.com>
2022-01-22 21:01:13 +0530

    [Shuvam] initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddressBookApp
+{
+    internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("#########################################");
+            Console.WriteLine("Hello From the Console AddressBook app!");
+            Console.WriteLine("#########################################\n");
+        }
+	}
+}
